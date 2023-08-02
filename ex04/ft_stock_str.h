24c58a3199d089429b14b1a633386cd2e92dfe40
stesfai <samsom.tesfai@pm.me>
2023-08-02 14:36:37 +0200

    Giving up

---
+++ ex04/ft_stock_str.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stesfai <stesfai@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/02 14:06:34 by stesfai           #+#    #+#             */
+/*   Updated: 2023/08/02 14:32:57 by stesfai          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}	t_stock_str;
+#endif
--- ex04/ft_strs_to_tab.c
+++ ex04/ft_strs_to_tab.c
@@ -6,7 +6,43 @@
 /*   By: stesfai <stesfai@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 13:10:04 by stesfai           #+#    #+#             */
-/*   Updated: 2023/08/02 13:10:05 by stesfai          ###   ########.fr       */
+/*   Updated: 2023/08/02 14:36:26 by stesfai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_stock_str.h"
+#include <stdlib.h> //Required for free() &malloc()
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+struct	s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int			i;
+	t_stock_str	s;
+
+	i = 0;
+	s.size = 1;
+	s.str = "Fuck";
+	s.copy = "f";
+	while (i < ac)
+	{
+		i++;
+	}
+	return (s);
+}
+
+/*Used for debugging*/
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	ft_strs_to_tab(ac, av);
+}
