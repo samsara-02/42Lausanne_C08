484839525c9b18d6c73bacfe5779e21262559bfc
stesfai <samsom.tesfai@pm.me>
2023-08-02 13:07:09 +0200

    Removed trash files

--- ex02/main.c
+++ ex02/main.c
@@ -5,30 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: stesfai <stesfai@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/27 16:45:30 by stesfai           #+#    #+#             */
-/*   Updated: 2023/08/02 13:03:29 by stesfai          ###   ########.fr       */
+/*   Created: 2023/08/02 12:01:13 by stesfai           #+#    #+#             */
+/*   Updated: 2023/08/02 12:15:12 by stesfai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_boolean.h"
+#include "ft_abs.h"
+#include <stdio.h>
 
-void	ft_putstr(char *str)
+int	main(void)
 {
-	while (*str)
-		write(1, str++, 1);
-}
+	int	x;
+	int	y;
 
-t_bool	ft_is_even(int nbr)
-{
-	return ((EVEN(nbr)) ? TRUE : FALSE);
-}
-
-int main(int argc, char **argv)
-{	
-	(void)argv;
-	if (ft_is_even(argc - 1) == TRUE)
-		ft_putstr(EVEN_MSG);
-	else
-		ft_putstr(ODD_MSG);
-	return (SUCCESS);
+	x = 42;
+	y = -42;
+	printf("Absolute value of %d is:	%d\n", x, ABS(x));
+	printf("Absolute value of %d is:	%d\n", y, ABS(y));
+	printf("Absolute value of %d is:	%d\n", 0, ABS(0));
 }
